ce13aa537dcb7c0f99b560dc0f8401688254f570
xiaojun du <xiaojun@id.iit.edu>
2017-04-27 22:30:39 -0500

    jquery assignment

--- part-1/script.js
+++ part-1/script.js
@@ -1,1 +1,21 @@
 // Do your work in this file.
+console.log("hello");
+ $("body").append("<div class='view'></div>");
+ $(".view").append("<div class='grass'></div>");
+ $(".view").append("<div class='sun'></div>");
+
+ var n=0;
+ var rayNode;
+ while(n++ < 5){
+    var rayDiv = "<div class='ray-" + n +  "'></div>";
+    $(".sun").append(rayDiv);
+ }
+
+ $(".view").append("<div class='mountain'></div>");
+ $(".mountain").append("<div class='mountain-top'></div>");
+
+ n=0;
+ while(n++ < 3){
+    var mountainCapDiv = "<div class='mountain-cap-" + n +  "'></div>";
+    $(".mountain-top").append(mountainCapDiv);
+ }
--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,18 @@
 // Do your work in this file.
+for(var i=0; i< 196; i++){
+    if(i%2){//even number
+        $("body").append("<div class='box type-1'></div>");
+    }else{//odd number
+        $("body").append("<div class='box type-2'></div>");
+    }
+}
+
+var box = $('.box');
+
+function boxClicked(){
+    //toggle class to clicked
+   console.log(this);
+   $(this).toggleClass("clicked");
+}
+
+box.click(boxClicked);
